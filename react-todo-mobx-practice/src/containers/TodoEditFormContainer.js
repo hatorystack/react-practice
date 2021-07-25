58e0d4c47f5d06ca7e176cf4f1c06c6fb918ca57
Helper Yoo <helper@purding.com>
2021-07-26 08:29:20 +0900

    Add react-todo-mobx-practice

---
+++ react-todo-mobx-practice/src/containers/TodoEditFormContainer.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import TodoEditFormView from '../views/TodoEditFormView';
+
+import { inject, observer } from 'mobx-react';
+import autobind from 'autobind-decorator';
+import generateId from '../IDGenerator';
+
+@inject('todoStore')
+@autobind
+@observer
+class TodoEditFormContainer extends Component {
+  onSetTodoProps(name, value) {
+    this.props.todoStore.setTodoProps(name, value);
+  }
+
+  onAddTodo() {
+    let { todo } = this.props.todoStore;
+    todo = { ...todo, id: generateId(5) };
+    this.props.todoStore.addTodo(todo);
+  }
+
+  onUpdateTodo() {
+    this.props.todoStore.updateTodo();
+  }
+
+  onRemoveTodo() {
+    this.props.todoStore.removeTodo();
+  }
+
+  render() {
+    const { todoStore } = this.props;
+
+    console.log(todoStore.todo);
+
+    return (
+      <TodoEditFormView
+        todo={todoStore.todo}
+        onSetTodoProps={this.onSetTodoProps}
+        onAddTodo={this.onAddTodo}
+        onUpdateTodo={this.onUpdateTodo}
+        onRemoveTodo={this.onRemoveTodo}
+      />
+    );
+  }
+}
+
+export default TodoEditFormContainer;
